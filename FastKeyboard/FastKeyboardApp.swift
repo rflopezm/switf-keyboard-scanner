9ab263a4f36395b6c86e9ae486671ca302c33203
Rodrigo Lopez <rflopezm@gmail.com>
2023-06-09 07:37:43 -0400

    Initial Commit

---
+++ FastKeyboard/FastKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastKeyboardApp.swift
+//  FastKeyboard
+//
+//  Created by Rodrigo on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FastKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
